418f7f436e8ec46e7f6b6eb952e0aaf61345f7ee
Zach Richard <zach.richard94@gmail.com>
2019-09-05 20:08:51 -0400

    working

--- app/app.ino
+++ app/app.ino
@@ -5,17 +5,17 @@ void blink(unsigned wait) {
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(wait);                       // wait 
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(wait);  
+  delay(200);  
 }
 
 void dot(unsigned repeats) {
   for (unsigned i = 0; i < repeats; i++) 
-    blink(250);
+    blink(200);
 }
 
 void dash(unsigned repeats) {
   for (unsigned i = 0; i < repeats; i++) 
-    blink(500);
+    blink(600);
 }
 
 // Blink out the morse code for a single input character. 
@@ -63,6 +63,7 @@ void char_to_morse(char c) {
     case '9': dash(4); dot(1); break;
     case '0': dash(5); break;
     default:
+      delay(500);
       Serial.print("Received an invalid character!: [");
       Serial.print(c);
       Serial.println("]");
@@ -73,27 +74,45 @@ void char_to_morse(char c) {
  * Main Setup
  */
 void setup() {
-	  // initialize the LED Pin for output
-    pinMode(LED_BUILTIN, OUTPUT);
-    
     // initialize the serial port - to capture input
     Serial.begin(9600);
+    delay(50);
+    
+    // initialize the LED Pin for output
+    pinMode(LED_BUILTIN, OUTPUT);
 
+
+    while(!Serial) {}
+    
     Serial.println("Setup Complete!");
+}
+
+bool validate_string(String s) {
+  if (s.length() < 1) {
+    Serial.println("Invalid string! Length: 0");
+    return false;
+  }
+  return true;
 }
 
 /* 
  * The Main Loop
  */
 void loop() {
-  //Serial.setTimeout(20000); // Set input timeout to 20 seconds (really long)
-  Serial.println("Enter a string to output in morse code (a-z, 0-9): ");
-  String input_string = Serial.readString();
-  Serial.print("Got string: ");
-  Serial.println(input_string);
+  bool valid_input = false;
+  String input_string = "";
+  while (!valid_input) {
+    //Serial.setTimeout(20000); // Set input timeout to 20 seconds (really long)
+    Serial.println("Enter a string to output in morse code (a-z, 0-9): ");
+    input_string = Serial.readString();
+    input_string.trim();
+    input_string.toLowerCase();
+    Serial.print("Got string: ");
+    Serial.println(input_string);
+    valid_input = validate_string(input_string);
+  }
   Serial.println("Outputing String... press ctrl-z to exit.");
-
-  Serial.setTimeout(1500);
+  //Serial.setTimeout(1500);
   unsigned input_char = (unsigned)0;
   // Start over on Control-Z
   while (input_char != 0x1A) { 
@@ -101,7 +120,13 @@ void loop() {
     for (int char_index = 0; char_index < input_string.length(); char_index++) {
       // blink morse code for each character
       char_to_morse(input_string.charAt(char_index));
+      input_char = Serial.read();
+      Serial.print("checking for ctrl-z... Got val: ");
+      Serial.print(input_char);
+      Serial.println();
+      if (input_char == 0x1A) {
+        break;
+      }
     }
-    input_char = Serial.read();
   }
 }
